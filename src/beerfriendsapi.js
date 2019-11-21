8f2182a4fa71a9475e5147b22cbf7db9e8ed3d79
Student <example@example.com>
2019-11-21 13:55:28 -0800

    add second joke api to project

--- src/beerfriendsapi.js
+++ src/beerfriendsapi.js
@@ -9,3 +9,15 @@ export class ApiCall {
     }
   }
 }
+
+export class JokeCall {
+  async getJokeData(search){
+    try {
+      let response2 = await fetch(`https://sv443.net/jokeapi/category/${search}`);
+      let jsonifiedResponse2 = await response2.json();
+      return jsonifiedResponse2;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Beer } from './../src/beerfriends.js';
 import {ApiCall} from './../src/beerfriendsapi.js';
+import {JokeCall} from './../src/beerfriendsapi.js';
 // import {MapsApiCall} from './../src/beerfriendsapi.js';
 
 $(document).ready(function() {
   $('#beers').submit(function(event) {
     event.preventDefault();
     const abvLevel = $('input[name=abv]:checked').val();
+    const jokeLevel = $('input[name=joke]:checked').val();
     let beer = new Beer(abvLevel);
+
 
     const getElements = function(response) {
       $('.showBeerName').text(`${response.name}`);
@@ -25,6 +28,22 @@ $(document).ready(function() {
       let jsonifiedResponse = await apiCall.getApiData(beer.searchQuery);
       let randomNumber = (Math.floor(Math.random() * jsonifiedResponse.length));
       getElements(jsonifiedResponse[randomNumber]);
+    })();
+
+    const getElements2 = function(response2) {
+      if(response2.joke){
+        $('.showJoke').text(`${response2.joke}`);
+      }
+      else{
+        $('.showSetup').text(`${response2.setup}`);
+        $('.showDelivery').text(`${response2.delivery}`);
+      }
+    };
+
+    (async () => {
+      let jokeCall = new JokeCall();
+      let jsonifiedResponse2 = await jokeCall.getJokeData(jokeLevel);
+      getElements2(jsonifiedResponse2);
     })();
 
   });
